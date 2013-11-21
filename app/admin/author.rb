dee98a76282abd3cf4891c29d646c3ece159190d
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2013-11-21 02:29:04 +0200

    minor administration changes

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Author do
+  index do                            
+    column :email                     
+    column :name
+    column :provider
+    column :created_at      
+    column :last_sign_in_at
+    column :current_sign_in_ip           
+    default_actions                   
+  end
 
 end
--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,11 +4,27 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+         span :class => "blank_slate" do
+            span I18n.t("active_admin.dashboard_welcome.welcome")
+            small I18n.t("active_admin.dashboard_welcome.call_to_action")
+          end
     end
+
+columns do
+  column do
+    panel "New Registrations" do
+      para "Latest users that registrered for a new account"
+      
+    end
+  end
+  end
+
+
+columns do
+  column do
+    panel "Latest Reviews" 
+  end
+end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
